a4a9aeab7e7a4c145ac20b1146a525658ba12ee6
Rajarshi Datta <rajarshi.datta@incture.com>
2018-10-31 11:16:57 +0530

    changing service dependency

--- ZpContainer/Zp_Rest/src/main/java/com/incture/zp/demo/controller/TestController.java
+++ ZpContainer/Zp_Rest/src/main/java/com/incture/zp/demo/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.incture.delfi.dto.ResponseDto;
+import com.incture.zp.demo.dto.ResponseDto;
 
 @RestController
 @ComponentScan("com.incture.zp.demo")
