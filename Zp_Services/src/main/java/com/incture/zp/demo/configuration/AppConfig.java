fd12896057a72376ba94576ce0ef6c2e594d2488
Rajarshi Datta <rajarshi.datta@incture.com>
2018-10-31 09:26:00 +0530

    folder structure commit

---
+++ Zp_Services/src/main/java/com/incture/zp/demo/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.incture.zp.demo.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.incture.zp.demo")
+public class AppConfig {
+
+}
