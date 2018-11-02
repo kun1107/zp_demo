491f790034f1aafa4221ac0713f49288823390ca
Rajarshi Datta <rajarshi.datta@incture.com>
2018-11-02 17:26:24 +0530

    adding persistence details

--- Zp_Services/src/main/java/com/incture/zp/demo/entity/TravelExpenseAttendeesDo.java
+++ Zp_Services/src/main/java/com/incture/zp/demo/entity/TravelExpenseAttendeesDo.java
@@ -1,7 +1,26 @@
 package com.incture.zp.demo.entity;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Entity
+@Data // for auto generation of getters and setters
+@Table(name = "TRAVEL_EXPENSE_ATTENDEES")
 public class TravelExpenseAttendeesDo implements BaseDo{
 	
 	/**
@@ -9,18 +28,30 @@ public class TravelExpenseAttendeesDo implements BaseDo{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ATTENDEES_ID")
 	private Long attendeesId;
 	
+	@Column(name = "ATTENDEE_TYPE")
 	private String attendeeType;
 	
+	@Column(name = "NAME")
 	private String name;
 	
+	@Column(name = "ATTENDEE_TITLE")
 	private String attendeeTitle;
 	
+	@Column(name = "DEPARTMENT")
 	private String department;
 	
+	@Column(name = "DATE")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date date;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "EXPENSE_LINE_ITEM_ID", nullable = false, updatable = false)
+	private TravelExpenseLineItemDetailsDo travelExpenseLineItemDetailsDo;
 	@Override
 	public Object getPrimaryKey() {
 		// TODO Auto-generated method stub
--- Zp_Services/src/main/java/com/incture/zp/demo/entity/TravelExpenseLineItemDetailsDo.java
+++ Zp_Services/src/main/java/com/incture/zp/demo/entity/TravelExpenseLineItemDetailsDo.java
@@ -4,6 +4,23 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Entity
+@Data // for auto generation of getters and setters
+@Table(name = "TRAVEL_EXPENSE_LINE_ITEM_DETAILS")
 public class TravelExpenseLineItemDetailsDo implements BaseDo {
 
 	/**
@@ -11,38 +28,58 @@ public class TravelExpenseLineItemDetailsDo implements BaseDo {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "EXPENSE_LINE_ITEM_ID")
 	private Long expenseLineItemId;
 	
+	@Column(name = "EXPENSE_TYPE")
 	private String expenseType;
 	
+	@Column(name = "DEPARTMENT")
 	private String department;
 	
+	@Column(name = "TRANSACTION_DATE")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date transactionDate;
 	
+	@Column(name = "BUSINESS_PURPOSE")
 	private String businessPurpose;
 	
+	@Column(name = "AMOUNT")
 	private BigDecimal amount;
 	
+	@Column(name = "AMOUNT_CURRENCY")
 	private String amountCurrency;
 	
+	@Column(name = "TAX")
 	private BigDecimal tax;
 	
+	@Column(name = "TAX_CURRENCY")
 	private String taxCurrency;
 	
+	@Column(name = "VENDOR")
 	private String vendor;
 	
+	@Column(name = "VENDOR_ADDRESS")
 	private String vendorAddress;
 	
+	@Column(name = "VENDOR_TIN")
 	private String vendorTin;
 	
+	@Column(name = "PAYMENT_TYPE")
 	private String paymentType;
 	
+	@Column(name = "COST_CENTRE_NAME")
 	private String costCentreName;
 	
+	@Column(name = "APPROVED_AMOUNT")
 	private BigDecimal approvedAmount;
 	
+	@Column(name = "COMMENTS")
 	private String comments;
 	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "travelExpenseLineItemDetailsDo")
 	private List<TravelExpenseAttendeesDo> listOfAttendees;
 	
 	@Override
